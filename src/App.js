82b9e1989e8d9b1a20347251125ebe2b499bb16a
Nate Korous <natekorous@workfront.com>
2018-08-29 15:04:26 -0600

    wrote connection for http calls. Need to create service component or something that takes a prop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Reports from "./components/reports.js"
+import Landing from './components/landing.js'
+import axios from 'axios'
+
 
 class App extends Component {
+  state = {
+    users: []
+  }
+
+  async getUsers(){
+    axios.get(`https://monkey-turtle.herokuapp.com/dbTest`)
+      .then((data) => {
+        data.data.forEach((records) => {
+          this.state.users.push({records})
+        })
+      })
+  }
+
+  componentDidMount(){
+    this.getUsers()
+  }
+
   render() {
     return (
+      //wrapping everything in router
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          Users: <br/>
+
+        </div>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Landing}/>
+            <Route path='/reports' component={Reports}/>
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/components/reports.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+
+class Reports extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Reports!!</h1>
+                <ul>
+                    <ul>
+                        <li>One</li>
+                        <li>Two</li>
+                        <li>THREE</li>
+                    </ul>
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+export default Reports
