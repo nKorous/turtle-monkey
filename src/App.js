6ade10cf69082a53927b7471f60f8b167b3eadf2
Nate Korous <natekorous@workfront.com>
2018-09-04 10:52:04 -0600

    updated CSS for main page and implemented static routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import Reports from "./components/reports.js"
-import Landing from './components/landing.js'
-import AdministrationHome from './components/administration/administration-home.js'
-
+import Reports from "./components/reports.js";
+import Landing from "./components/landing.js";
+import AdministrationHome from "./components/administration/administration-home.js";
 
 class App extends Component {
-  constructor(props) { //The constructor handles State. when state changes, it kicks off a re-render
-    super(props)
+  constructor(props) {
+    //The constructor handles State. when state changes, it kicks off a re-render
+    super(props);
     this.state = {
       users: []
-    }
-
+    };
   }
 
   render() {
@@ -24,14 +23,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div>
-        </div>
+
         <Router>
+          <div>
+          <div className="linkBar">
+          <Link className='linkButton' to="/">Home</Link>
+            <Link className='linkButton' to="/reports">Reports</Link>
+            <Link className='linkButton' to="/administration">Administration</Link>
+          </div>
           <Switch>
-            <Route exact path='/' component={Landing}/>
-            <Route path='/reports' component={Reports}/>
-            <Route path='/administration' component={AdministrationHome} />
+            <Route exact path="/" component={Landing} />
+            <Route path="/reports" component={Reports} />
+            <Route path="/administration" component={AdministrationHome} />
           </Switch>
+          </div>
         </Router>
       </div>
     );
